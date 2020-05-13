fc44f06979728343fae042784b08fc36a186a318
retiredbusdriver <retiredbusdriver@tutanota.com>
2020-05-13 12:40:22 -0600

    First attempt and failture at automating game() function

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function playerTest() {
-  let rpsSpin = ['rock', 'paper', 'scissors'];
-  let ranRpsNum = Math.floor(Math.random() * 3);
-  return rpsSpin[ranRpsNum];
-}
-
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
   let rpsSpin = ['rock', 'paper', 'scissors'];
@@ -11,16 +7,17 @@ function computerPlay() {
   return rpsSpin[ranRpsNum];
 }
 
-
 function playRound(playerSelection, computerSelection) {
   if ((playerSelection === 'rock' && computerSelection === 'paper') ||
       (playerSelection === 'paper' && computerSelection === 'scissors') ||
       (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        computerScore++;
         return `Player loses, ${computerSelection} beats ${playerSelection}`
       }
   else if ((playerSelection === 'paper' && computerSelection === 'rock') ||
       (playerSelection === 'scissors' && computerSelection === 'paper') ||
       (playerSelection === 'rock' && computerSelection === 'scissors')) {
+        playerScore++;
         return `Player wins, ${playerSelection} beats ${computerSelection}`
       }
   else {
@@ -28,6 +25,14 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-playerSelection = prompt('Enter rock, paper, or scissors');
+function game() {
+  let playerSelection = prompt('Enter rock, paper, or scissors').toLowerCase();
 
-console.log(playRound(playerSelection, computerPlay()));
+  for(let i = 0; i > 5; i++) {
+    playRound(playerSelection, computerPlay());
+    return i;
+  }
+
+}
+
+console.log(game());
