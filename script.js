786beb6a2a5cb9c2a308c6cf66f4edd3ed7fd0ce
retiredbusdriver <retiredbusdriver@tutanota.com>
2020-07-03 12:24:50 -0600

    Finished without proper counter

--- script.js
+++ script.js
@@ -1,38 +1,122 @@
+const playerParaScore = document.querySelector('p#playerScore');
+const computerParaScore = document.querySelector('p#computerScore');
+const counters = document.querySelector('div.counters');
+const selectedChoices = document.querySelector('p#selectedChoices');
+
+let playerSelected = '';
+let computerSelected = '';
+let holderPick = '';
 let playerScore = 0;
 let computerScore = 0;
+let totalScore = 0;
+
 
 function computerPlay() {
-  let rpsSpin = ['rock', 'paper', 'scissors'];
+  let rpsSpin = ["rock", "paper", "scissors"];
   let ranRpsNum = Math.floor(Math.random() * 3);
   return rpsSpin[ranRpsNum];
 }
 
+
+document.addEventListener('click', function(e) {
+    console.log(e.originalTarget.id);
+    if ((playerScore == 5 && playerScore > computerScore) ||
+        (computerScore == 5 && playerScore < computerScore)) {
+      gameSet();
+      ++playerScore;
+      ++computerScore;
+    }
+    else if (e.originalTarget.id == 'rockImg' && (computerScore < 5 && playerScore < 5)) {
+      holderPick = 'rock';
+      game();
+      currentComputerScorePara.textContent = `${computerScore}`;
+      currentPlayerScorePara.textContent = `${playerScore}`
+    }
+    else if (e.originalTarget.id == 'paperImg' && (computerScore < 5 && playerScore < 5)) {
+      holderPick = 'paper';
+      game();
+      currentComputerScorePara.textContent = `${computerScore}`;
+      currentPlayerScorePara.textContent = `${playerScore}`
+    }
+    else if (e.originalTarget.id == 'scissorsImg' && (computerScore < 5 && playerScore < 5)) {
+      holderPick = 'scissors';
+      game();
+      currentComputerScorePara.textContent = `${computerScore}`;
+      currentPlayerScorePara.textContent = `${playerScore}`
+    }
+  });
+
+
 function playRound(playerSelection, computerSelection) {
-  if ((playerSelection === 'rock' && computerSelection === 'paper') ||
-      (playerSelection === 'paper' && computerSelection === 'scissors') ||
-      (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        computerScore++;
-        return `Player loses, ${computerSelection} beats ${playerSelection}`
-      }
-  else if ((playerSelection === 'paper' && computerSelection === 'rock') ||
-      (playerSelection === 'scissors' && computerSelection === 'paper') ||
-      (playerSelection === 'rock' && computerSelection === 'scissors')) {
-        playerScore++;
-        return `Player wins, ${playerSelection} beats ${computerSelection}`
-      }
-  else {
-    return 'It was a tie!';
+  playerSelected = playerSelection;
+  computerSelected = computerSelection;
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    return "lose";
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "rock" && computerSelection === "scissors")
+  ) {
+    return "win";
   }
 }
 
 function game() {
-  let playerSelection = prompt('Enter rock, paper, or scissors').toLowerCase();
-
-  for(let i = 0; i > 5; i++) {
-    playRound(playerSelection, computerPlay());
-    return i;
-  }
-
+      let gameState = playRound(holderPick, computerPlay());
+        if (gameState === "win") {
+          ++playerScore;
+          ++totalScore;
+          selectedChoices.textContent = `Player selected ${playerSelected} and computer selected ${computerSelected}`
+          console.log(`Player's ${playerSelected} beats computer's ${computerSelected}, player wins this round. Player is at ${playerScore} and computer is at ${computerScore}`)
+        } else if (gameState === "lose") {
+          ++computerScore;
+          ++totalScore;
+          selectedChoices.textContent = `Computer selected ${computerSelected} and player selected ${playerSelected}`
+          console.log(`Computer's ${computerSelected} beats player's ${playerSelected}, player loses this round. Player is at ${playerScore} and computer is at ${computerScore}`)
+        } else {
+          ++totalScore;
+          selectedChoices.textContent = `Both computer and player selcted ${playerSelected}`
+          console.log(`Both computer and player selected ${playerSelected}, it was a tie! Player is at ${playerScore} and computer is at ${computerScore}`)
+        }
+  if (playerScore > computerScore) {
+      return `Player wins! Player had a score of ${playerScore} and computer had ${computerScore} over ${totalScore} rounds.`;
+    }
+  else if (playerScore < computerScore) {
+      return `Player loses! Player had a score of ${playerScore} and computer had ${computerScore} over ${totalScore} rounds.`;
+    }
 }
 
-console.log(game());
+const currentPlayerScore = playerScore;
+const currentComputerScore = computerScore;
+let currentPlayerScorePara = document.createElement('p');
+currentPlayerScorePara.classList.add('currentPlayerScorePara');
+currentPlayerScorePara.textContent = `${playerScore}`
+playerParaScore.appendChild(currentPlayerScorePara);
+
+let currentComputerScorePara = document.createElement('p');
+currentComputerScorePara.classList.add('currentComputerScorePara');
+currentComputerScorePara.textContent = `${computerScore}`;
+computerParaScore.appendChild(currentComputerScorePara);
+
+function gameSet() {
+  if (playerScore === 5) {
+    currentComputerScorePara.textContent = '';
+    currentPlayerScorePara.textContent = '';
+    const newPlayerScore = document.createElement('p');
+    newPlayerScore.classList.add('newPlayerScore');
+    newPlayerScore.textContent = `Player won with ${playerScore} wins to ${computerScore} over ${totalScore} rounds!`
+    playerParaScore.appendChild(newPlayerScore);
+  }
+  else if (computerScore === 5) {
+    currentComputerScorePara.textContent = '';
+    currentPlayerScorePara.textContent = '';
+    const newComputerScore = document.createElement('p');
+    newComputerScore.classList.add('newComputerScore');
+    newComputerScore.textContent = `Computer won with ${computerScore} wins to ${playerScore} over ${totalScore} rounds!`
+    computerParaScore.appendChild(newComputerScore);
+  }
+}
